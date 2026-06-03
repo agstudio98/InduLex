7bf8e3325622193e465bb39dd7d92539b7c19f5e
Ag Studio <libertadoraguirre@gmail.com>
2026-06-03 16:37:31 -0300

    Change server listen address to '0.0.0.0'

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 let server;
 if (process.env.NODE_ENV !== 'test') {
-    server = app.listen(PORT, () => {
+    server = app.listen(PORT, '0.0.0.0', () => {
         logger.info(`Server running on port ${PORT}`);
     });
 }
